ca9e357d5c9d8278de3b6153958e5e337477be29
Владас Куодис <vlad250577@yandex.ru>
2025-12-27 21:29:51 +0300

    Update LANGUAGE_CODE to Russian and enhance MeterReadingAdmin with fieldsets

--- kommunalka_calc/meters/admin.py
+++ kommunalka_calc/meters/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import MeterReading
 
-# Register your models here.
+@admin.register(MeterReading)
+class MeterReadingAdmin(admin.ModelAdmin):
+    list_display = (
+        "created_at",
+        "cold_prev", "cold_curr",
+        "hot_prev", "hot_curr",
+        "electricity", "internet",
+        "total",
+    )
+
+    list_filter = ("created_at",)
+
+    ordering = ("-created_at",)
+
+    readonly_fields = (
+        "cold_cost",
+        "hot_cost",
+        "sewage_cost",
+        "total",
+        "created_at",
+    )
+
+    fieldsets = (
+        ("Показания", {
+            "fields": (
+                ("cold_prev", "cold_curr"),
+                ("hot_prev", "hot_curr"),
+            )
+        }),
+        ("Дополнительно", {
+            "fields": ("electricity", "internet")
+        }),
+        ("Результат", {
+            "fields": (
+                "cold_cost",
+                "hot_cost",
+                "sewage_cost",
+                "total",
+                "created_at",
+            )
+        }),
+    )
+
